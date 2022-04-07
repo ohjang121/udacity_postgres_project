27404afd18a596cc2acf65988d8718eb52e049b4
Jo Oh <joh@hubspot.com>
2022-04-07 04:14:53 -0400

    adding completed & tested project

---
+++ README.md
@@ -0,0 +1,59 @@
+# Project 1: Data Modeling with Postgres
+
+Objective: Build an ETL pipeline for a music app (Sparkify) using Python & Postgres. Two datasets used are both in JSON format, and key parts are extracted from the datasets to build a star schema optimized for queries on song play analysis.
+
+## Schema & Table Design
+
+The database name is `sparkifydb`. The project's star schema contains 1 fact table and 4 dimension tables - create, insert, and drop SQL logic defined in `sql_queries.py`:
+
+### Fact Table(s)
+
+1. **songplays** - records in log data associated with song plays i.e. records with page `NextSong`
+    - columns: *songplay_id (pkey), start_time (foreign key to **time**), user_id (foreign key to **users**), level, song_id (foreign key to **songs**, artist_id (foreign key to **artists**), session_id, location, user_agent*
+    
+### Dimension Table(s)
+
+1. **users** - users in the app
+    - columns: *user_id (pkey), first_name, last_name, gender, level*
+2. **songs** - songs in music database
+    - columns: *song_id (pkey), title, artist_id, year, duration*
+3. **artists** - artists in music database
+    - columns: *artist_id (pkey), name, location, latitude, longitude*
+4. **time** - timestamps of records in **songplays** broken down into specific units
+    - columns: *start_time (pkey), hour, day, week, month, year, weekday*
+    
+`create_tables.py` creates and connects to the `sparkify` database to build the shell tables so that data can be loaded into the tables.
+
+## ETL Process
+
+`etl.ipynb` contains the step-by-step guidance to build the ETL Process for `sparkifydb`, which gets formalized in `etl.py` for execution. The two datasets populate the following tables:
+
+* `data/song_data`: songs, artists
+* `data/log_data` time, users
+* combined: songplays
+
+Note that all data in `data/song_data` only have 1 row per file, so the logic only has to load the first index (values[0]) per file iteration. Each file in `data/log_data` contains several lines, hence iteration per file is required to load all data into the tables.
+
+## Steps to run the scripts
+
+1. The Project Workplace already has necessary libraries installed, but for a brand new environment, it is required to install all libraries that are not built-in (e.g. psycopg2).
+2. Run `create_tables.py` to create shell tables: 
+```
+python create_tables.py
+```
+3. Run `etl.py` to perform the ETL Process
+```
+python etl.py
+```
+
+## Example Queries
+
+* How many distinct users are Male?
+
+`select count(distinct user_id) from users where gender = 'M'`
+
+* List mobile user_agents
+
+`select user_agent from songplays where user_agent ilike '%mobile%' group by 1`
+
+
---
+++ sql_queries.py
@@ -0,0 +1,135 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, -- autoincrement surrogate key, not in the log file
+                                      start_time TIMESTAMP NOT NULL, -- log.ts
+                                      user_id INT NOT NULL, -- log.userId
+                                      level VARCHAR, -- log.level
+                                      song_id VARCHAR, -- song.song_id
+                                      artist_id VARCHAR, -- song.artist_id
+                                      session_id INT, -- log.sessionId
+                                      location VARCHAR, -- log.location
+                                      user_agent VARCHAR -- log.userAgent
+                                      );
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, -- log.userId 
+                                  first_name VARCHAR, -- log.firstName
+                                  last_name VARCHAR, -- log.lastName
+                                  gender VARCHAR, -- log.gender
+                                  level VARCHAR -- log.level
+                                  );
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR PRIMARY KEY, -- song.song_id
+                                  title VARCHAR NOT NULL, -- song.title, log.song
+                                  artist_id VARCHAR, -- song.artist_id
+                                  year INT, -- song.year
+                                  duration FLOAT NOT NULL -- song.duration, log.length (maybe)
+                                  );
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR PRIMARY KEY, -- song.artist_id
+                                    name VARCHAR NOT NULL, -- song.artist_name
+                                    location VARCHAR, -- song.artist_location
+                                    latitude FLOAT, -- song.artist_latitude
+                                    longitude FLOAT -- song.artist_longitude
+                                    );
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (start_time BIGINT PRIMARY KEY, -- log.ts
+                                 hour INT, 
+                                 day INT,
+                                 week INT,
+                                 month INT,
+                                 year INT,
+                                 weekday INT
+                                 );
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplays (songplay_id, 
+                       start_time,
+                       user_id,
+                       level,
+                       song_id,
+                       artist_id, 
+                       session_id, 
+                       location, 
+                       user_agent)
+VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, 
+                   first_name, 
+                   last_name, 
+                   gender, 
+                   level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id,
+                   title, 
+                   artist_id, 
+                   year, 
+                   duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, 
+                     name, 
+                     location, 
+                     latitude, 
+                     longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+time_table_insert = ("""
+INSERT INTO time (start_time, 
+                  hour, 
+                  day, 
+                  week, 
+                  month, 
+                  year, 
+                  weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT s.song_id,
+a.artist_id
+FROM songs s
+JOIN artists a on s.artist_id = a.artist_id
+WHERE s.title = %s
+AND a.name = %s
+AND s.duration = %s;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
